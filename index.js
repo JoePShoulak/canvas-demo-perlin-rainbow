9d0fc6ea426a45973e7a46e22a5ca469cb9e339e
Joe P Shoulak <joe@squarecompass.io>
2022-10-22 00:06:32 -0500

    perlin movement implmented, missing screen restriction

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { resizeCanvas, rotate } from "./lib/helper.js";
 import { Particle } from "./lib/Particle.js";
+import { noise } from "./lib/Perlin.js";
 
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("my-canvas");
@@ -28,14 +29,34 @@ const setup = () => {
   }
 };
 
+// noise(1)
+let increment = 0;
+
 const animate = () => {
   requestAnimationFrame(animate);
 
   c.fillStyle = `rgba(0, 0, 0, ${bgAlpha})`;
   c.fillRect(0, 0, innerWidth, innerHeight);
 
+  center.x += noise(increment + 5 * Math.PI) - 0.5;
+  center.y += noise(increment + 10 * Math.PI) - 0.5;
+
+  // TODO: Keep this damn thing in the window
+  center.x = Math.min(center.x, innerWidth - 100);
+  center.x = Math.max(center.x, 100);
+  center.y = Math.min(center.y, innerHeight - 100);
+  center.y = Math.max(center.y, 100);
+
+  increment += 0.001;
+
   particles.forEach((particle) => {
-    const newPoint = rotate(center.x, center.y, particle.x, particle.y, 0.03);
+    const newPoint = rotate(
+      center.x,
+      center.y,
+      particle.x,
+      particle.y,
+      noise(increment) - 0.5
+    );
 
     [particle.x, particle.y] = newPoint;
 
